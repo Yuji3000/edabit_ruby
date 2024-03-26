da6e1ed7a9779a6e1b213be4641398c91ef24e75
Yuji Kosakowski <yuji.kosa@gmail.com>
2024-03-26 14:07:57 -0600

    Add new method to chunkfiy

--- lib/chunkify.rb
+++ lib/chunkify.rb
@@ -2,8 +2,17 @@
 #  where n is the length of each chunk.
 
 
+# def chunkify(array, n)
+#   array.each_slice(n).map do |nums|
+#     nums
+#   end
+# end
+
 def chunkify(array, n)
-  array.each_slice(n).map do |nums|
-    nums
-  end
+  array.each_slice(n).to_a
 end
+
+# for RAILS only
+# def chunkify(array, n)
+#   array.in_groups_of(n)
+# end
