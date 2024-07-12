300c2a42d222a3b66ef168d008c6d0412d437c3a
Yuji Kosakowski <yuji.k.dev@gmail.com>
2024-07-11 20:59:52 -0600

    Finished multiplicity challenge

---
+++ lib/multiplicity.rb
@@ -0,0 +1,12 @@
+# def multiplicity(arr)
+#   final_array = []
+#   arr.tally.each do |key, value|
+#     final_array << [key, value]
+#     end
+#   final_array
+# end
+
+# def multiplicity(arr)
+#   arr.uniq.map { |num| [num, arr.count(num)]}
+# end
+
